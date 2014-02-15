b291c4f3325406f69b21aff8466113b154a8c203
Shawn Resene <shawnresene@pluto.dsu.edu>
2014-02-14 20:50:45 -0600

    Task C

--- Chapter/config/environments/development.rb
+++ Chapter/config/environments/development.rb
@@ -1,12 +1,4 @@
-#---
-# Excerpted from "Agile Web Development with Rails",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
-#---
-Depot::Application.configure do
+Chapter::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -19,7 +11,7 @@ Depot::Application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
---
+++ Chapter/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chapter::Application.config.secret_key_base = 'c90250bf09fb372c2f0fdedcd314bc7c752ef1dbeabc3ff59a9790f4b637078b01385cc212f434f41603259d89af66ebfcaf0c566d2bf274b75c64b8ac5779ba'
